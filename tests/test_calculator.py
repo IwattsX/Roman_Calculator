70e9d422379ded47c18b612bdaedec59fbdde6a3
= <Isaac.s.watts@gmail.com>
2024-09-24 12:13:24 -0400

    Capture the return value with the test as well as the stdout

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,64 +8,71 @@ def test_single_roman_numeral(monkeypatch, capsys):
     monkeypatch.setattr('sys.argv', ['calculator', 'V'])
 
     # Call the main function
-    main()
+    val = main()
 
     # Capture the output
     captured = capsys.readouterr()
 
     # Check the output
     assert captured.out.strip() == '5'
+    assert val == 5
 
 def test_single_invalid_RN(monkeypatch, capsys):
     # Simulate the command line argument `./venv/bin/calculator V`
     monkeypatch.setattr('sys.argv', ['calculator', 'Q'])
 
     # Call the main function
-    main()
+    val = main()
 
     # Capture the output
     captured = capsys.readouterr()
 
     # Check the output
     assert captured.out.strip() == 'I don\'t know how to read this.'
+    assert val == 'I don\'t know how to read this.'
 
 def test_valid_expression(monkeypatch, capsys):
     monkeypatch.setattr('sys.argv', ['calculator', 'X', '+', 'V'])
 
-    main()
+    val = main()
     captured = capsys.readouterr()
 
     assert captured.out.strip() == 'XV'
+    assert val == "XV"
 
 def test_invalid_input(monkeypatch, capsys):
     monkeypatch.setattr('sys.argv', ['calculator', 'V', 'III'])
 
 
-    main()
+    val = main()
     captured = capsys.readouterr()
 
     assert captured.out.strip() == "I don\'t know how to read this."
+    assert val == "I don\'t know how to read this."
 
 def test_complex_expression(monkeypatch, capsys):
     monkeypatch.setattr('sys.argv', ['calculator', 'X', '+', 'X', '*', 'II'])
 
-    main()
+    val = main()
     captured = capsys.readouterr()
 
     assert captured.out.strip() == 'XXX'
+    assert val == "XXX"
 
 def test_no_args(monkeypatch, capsys):
     monkeypatch.setattr('sys.argv', ['calculator'])
 
-    main()
+    val = main()
 
     captured = capsys.readouterr()
     assert captured.out.strip() == "I don\'t know how to read this."
+    assert val == "I don\'t know how to read this."
 
 def test_invalid_operation(monkeypatch, capsys):
     monkeypatch.setattr('sys.argv', ['calculator', "2", "+", "2"])
 
-    main()
+    val = main()
 
     captured = capsys.readouterr()
     assert captured.out.strip() == "I don\'t know how to read this."
+    assert val == "I don\'t know how to read this."
